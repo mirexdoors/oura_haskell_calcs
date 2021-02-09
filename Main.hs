b2c9346215712ea600b1ce714c48fa6329eeb1cb
mikhail <prokudin.mi@sibvaleo.com>
2021-02-09 18:06:04 +0700

    add day health data type

--- Main.hs
+++ Main.hs
@@ -4,19 +4,48 @@ import Data.Aeson
 import qualified Data.ByteString.Lazy as B
 import GHC.Generics
 
-data Mathematician = Mathematician { name :: String,
-                                     nationality :: String,
-                                     born :: Int,
-                                     died :: Maybe Int
-                                   } deriving Generic
+data DayHealthItem = DayHealthItem { summary_date :: String,
+                                     period_id :: Int,
+                                     is_longest :: Int,
+                                     timezone :: Int,
+                                     bedtime_start :: String,
+                                     bedtime_end :: String,
+                                     score :: Int,
+                                     score_total :: Int,
+                                     score_disturbances :: Int,
+                                     score_efficiency :: Int,
+                                     score_latency :: Int,
+                                     score_rem :: Int,
+                                     score_deep :: Int,
+                                     score_alignment :: Int,
+                                     total :: Int,
+                                     duration :: Int,
+                                     awake :: Int,
+                                     light :: Int,
+                                     rem :: Int,
+                                     deep :: Int,
+                                     onset_latency :: Int,
+                                     restless :: Int,
+                                     efficiency :: Int,
+                                     midpoint_time :: Int,
+                                     hr_lowest :: Int,
+                                     hr_average :: Double,
+                                     rmssd :: Int,
+                                     breath_average :: Double,
+                                     temperature_delta :: Double,
+                                     hypnogram_5min :: String,
+                                     hr_5min :: [Int],
+                                     rmssd_5min :: [Int]
+                                   } deriving (Generic, Show)
 
-instance FromJSON Mathematician
+instance FromJSON DayHealthItem
 
+main :: [DayHealthItem]
 main = do
-  input <- B.readFile "mock/input.json"
-  let mm = decode input :: Maybe Mathematician
+  input <- B.readFile "mock/data.json"
+  let mm = decode input :: Maybe [DayHealthItem]
   case mm of
     Nothing -> print "error parsing JSON"
-    Just m -> (putStrLn.greet) m
+    Just m -> m
 
-greet m = (show.name) m ++" was born in the year "++ (show.born) m
+greet m = m
